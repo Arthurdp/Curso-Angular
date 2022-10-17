1e6088c2d29a38f3240856c6acc95cd8f18e7ab1
Arthur Dantas Porto <arthur.porto@ccc.ufcg.edu.br>
2022-10-16 23:32:31 -0300

    ...

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCommonModule} from '@angular/material/core';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCommonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
